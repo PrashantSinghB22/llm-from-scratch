0552084d5df568ffc7164e7e97a0a532a7a02a6d
Prashant Singh Balyan <235067837+PrashantSinghB22@users.noreply.github.com>
2026-08-07 13:43:37 +0530

    Implement multi-head attention

--- src/models/attention.py
+++ src/models/attention.py
@@ -51,3 +51,39 @@ class Head(nn.Module):
         out = wei @ v
 
         return out
+
+class MultiHeadAttention(nn.Module):
+
+    def __init__(
+        self,
+        num_heads,
+        head_size,
+        n_embed,
+        block_size
+    ):
+        super().__init__()
+
+        self.heads = nn.ModuleList([
+            Head(
+                n_embed,
+                head_size,
+                block_size
+            )
+            for _ in range(num_heads)
+        ])
+
+        self.proj = nn.Linear(
+            num_heads * head_size,
+            n_embed
+        )
+
+    def forward(self, x):
+
+        out = torch.cat(
+            [h(x) for h in self.heads],
+            dim=-1
+        )
+
+        out = self.proj(out)
+
+        return out
